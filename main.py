34f715bdc7d874b929873caf4783935abdeb499d
tuhin1522 <mdtuhin1499@gmail.com>
2026-06-13 22:39:18 +0600

    Implement data ingestion pipeline with configuration management and logging

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Starting the main.py script for Kidney Disease Classification.") 
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
