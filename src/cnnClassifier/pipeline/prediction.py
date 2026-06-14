a7e09208980feee01a95fc9f7c180c7d5361ed78
tuhin1522 <mdtuhin1499@gmail.com>
2026-06-14 17:56:59 +0600

    feat: add initial application structure with Flask, DVC configuration, and prediction pipeline

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,33 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+class PredictionPipeline:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def predict(self):
+        # Load the trained model
+        model = load_model(os.path.join('model', 'model.h5'))
+
+        # Load and preprocess the image
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = ''
+            return [{ "image" : prediction}]
+        elif result[1] == 2:
+            prediction = ''
+            return [{ "image" : prediction}]
+        elif result[2] == 3:
+            prediction = ''
+            return [{ "image" : prediction}]  
+        else:
+            prediction = ''
+            return [{ "image" : prediction}]
